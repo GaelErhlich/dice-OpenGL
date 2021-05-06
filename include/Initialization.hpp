9d31108672f57ceb8c5536598b58ff63d3478db7
GaelErhlich <idiotdu92@gmail.com>
2021-05-07 00:57:21 +0200

    Mineur

---
+++ include/Initialization.hpp
@@ -0,0 +1,5 @@
+
+#include <SDL2/SDL.h>;
+#include <SDL2/SDL_syswm.h>;
+
+bool initializeOpenGL(SDL_Window*& window, SDL_GLContext& context, int WIDTH, int HEIGHT);
---
+++ src/Initialization.cpp
@@ -0,0 +1,45 @@
+#include "Initialization.hpp"
+
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_syswm.h>
+#include <GL/glew.h>
+#include <GL/gl.h>
+
+
+bool initializeOpenGL(SDL_Window* &window, SDL_GLContext &context, int WIDTH, int HEIGHT) {
+    //Initialize SDL2
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK) < 0)
+    {
+        ERROR("The initialization of the SDL failed : %s\n", SDL_GetError());
+        return false;
+    }
+
+    //Create a Window
+    window = SDL_CreateWindow("VR Camera",                           //Titre
+        SDL_WINDOWPOS_UNDEFINED,               //X Position
+        SDL_WINDOWPOS_UNDEFINED,               //Y Position
+        WIDTH, HEIGHT,                         //Resolution
+        SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN); //Flags (OpenGL + Show)
+
+    //Initialize OpenGL Version (version 3.0)
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+
+    //Initialize the OpenGL Context (where OpenGL resources (Graphics card resources) lives)
+    context = SDL_GL_CreateContext(window);
+
+    //Tells GLEW to initialize the OpenGL function with this version
+    glewExperimental = GL_TRUE;
+    glewInit();
+
+
+    //Start using OpenGL to draw something on screen
+    glViewport(0, 0, WIDTH, HEIGHT); //Draw on ALL the screen
+    glClearColor(0.0, 0.0, 0.0, 1.0); //Full Black
+
+    glEnable(GL_DEPTH_TEST); //Active the depth test
+
+    return true;
+
+}
